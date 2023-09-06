d2750355c6af885de42cd801440b5350b9716cda
John Kalbac <john.kalbac@mastercard.com>
2023-09-06 13:48:23 -0500

    Initial commit.

--- src/main/java/com/reference/genai/sandbox/Application.java
+++ src/main/java/com/reference/genai/sandbox/Application.java
@@ -1,7 +1,11 @@
 package com.reference.genai.sandbox;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@RestController
+	public class SampleController {
+
+		@Value("${message:hey}")
+		private String message;
+
+		@GetMapping
+		public ResponseEntity<String> sayHello() {
+			return ResponseEntity.ok(message);
+		}
+
+	}
 }
